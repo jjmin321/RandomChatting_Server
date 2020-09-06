a3980c4ed3f466478ac9350275c8a2522f3b03f4
jjmin321 <jjmin321@naver.com>
2020-09-06 16:21:08 +0900

    [UPDATE] SocketServer.go

--- SocketServer.go
+++ SocketServer.go
@@ -1,53 +1,45 @@
 package main
 
 import (
-	"log"
-	"net/http"
+	"fmt"
 
-	"github.com/gorilla/pat"
 	"github.com/gorilla/websocket"
-	"github.com/urfave/negroni"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 )
 
-var upgrader = websocket.Upgrader{
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
-}
+var (
+	upgrader = websocket.Upgrader{}
+)
 
-type Message struct {
-	Type string      `json:"type"`
-	Data interface{} `json:"data"`
-}
-
-func wshandler(w http.ResponseWriter, r *http.Request) {
-	conn, err := upgrader.Upgrade(w, r, nil)
+func hello(c echo.Context) error {
+	ws, err := upgrader.Upgrade(c.Response(), c.Request(), nil)
 	if err != nil {
-		log.Println(err)
-		return
+		return err
 	}
+	defer ws.Close()
 
 	for {
-		m := &Message{}
-		err := conn.ReadJSON(m)
+		// Write
+		err := ws.WriteMessage(websocket.TextMessage, []byte("Hello, Client!"))
 		if err != nil {
-			log.Println(err)
-			return
+			c.Logger().Error(err)
 		}
 
-		err = conn.WriteJSON(m)
+		// Read
+		_, msg, err := ws.ReadMessage()
 		if err != nil {
-			log.Println(err)
-			return
+			c.Logger().Error(err)
 		}
+		fmt.Printf("%s\n", msg)
 	}
 }
 
 func main() {
-	mux := pat.New()
-	mux.Get("/ws", wshandler)
-
-	n := negroni.Classic()
-	n.UseHandler(mux)
-
-	http.ListenAndServe(":3000", n)
+	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.Static("/", "../public")
+	e.GET("/", hello)
+	e.Logger.Fatal(e.Start(":80"))
 }
