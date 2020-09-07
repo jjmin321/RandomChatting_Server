cb8420fa6a2afa1563bea8c509eb871f7120b571
jjmin321 <jjmin321@naver.com>
2020-09-07 11:53:39 +0900

    [UPDATE] SocketServer.go

--- SocketServer.go
+++ SocketServer.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"container/list"
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/gorilla/websocket"
@@ -16,19 +18,44 @@ const (
 	ROOM_MAX_COUNT = 50
 )
 
+// Client - 채팅을 이용하는 사용자의 정보
+type Client struct {
+	connection websocket.Conn
+	read       chan string
+	quit       chan int
+	name       string
+	room       *Room
+}
+
+// Room - 채팅방 정보
+type Room struct {
+	num        int
+	clientlist *list.List
+}
+
 var (
+	roomlist *list.List
 	upgrader = &websocket.Upgrader{
 		ReadBufferSize:  1024,
 		WriteBufferSize: 1024,
 	}
 )
 
-func hello(c echo.Context) error {
+func init() {
+	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.GET("/", socket)
+	e.Logger.Fatal(e.Start(":80"))
+}
+
+func socket(c echo.Context) error {
 	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
 	ws, err := upgrader.Upgrade(c.Response(), c.Request(), nil)
 	if err != nil {
 		return err
 	}
+	log.Print("채팅 서버를 80번 포트에 열었습니다.")
 	defer ws.Close()
 
 	for {
@@ -48,10 +75,11 @@ func hello(c echo.Context) error {
 }
 
 func main() {
-	e := echo.New()
-	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
-	// e.Static("/", "../public")
-	e.GET("/", hello)
-	e.Logger.Fatal(e.Start(":80"))
+	roomlist := list.New()
+	for i := 0; i < ROOM_MAX_COUNT; i++ {
+		room := &Room{i+1, list.New()}
+		roomlist.PushBack(*room)
+	}
+	chatting, err := websocket.
+
 }
