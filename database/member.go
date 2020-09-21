4026859e3f06c73a453b708f4ceff60cdbe80a59
jjmin321 <jjmin321@naver.com>
2020-09-21 19:48:07 +0900

    [DELETE] ID Attribute

--- database/member.go
+++ database/member.go
@@ -5,9 +5,8 @@ import "time"
 // Member 멤버 관리 테이블
 type Member struct {
 	Idx       uint      `gorm:"primary_key; auto_increment:true" json:"idx"`
-	UserID    string    `gorm:"type:varchar(255);unique;not null" json:"user_id"`
+	Name      string    `gorm:"type:varchar(255);not null" json:"name"`
 	Pw        string    `gorm:"type:varchar(255);not null" json:"pw"`
-	Nickname  string    `gorm:"type:varchar(255);not null" json:"nickname"`
 	Image     string    `gorm:"type:varchar(255);not null" json:"image"`
 	Megaphone uint      `gorm:"not null" sql:"DEFAULT:0" json:"megaphone"`
 	JoinedAt  time.Time `gorm:"not null" sql:"DEFAULT:current_timestamp" json:"joined_at"`
