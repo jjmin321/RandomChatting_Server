2f44606ca30dd86e816081e4190897f9b9ea9bdf
jjmin321 <jjmin321@naver.com>
2020-09-21 22:16:43 +0900

    [ADD] updateImage

---
+++ model/updateImage.go
@@ -0,0 +1,10 @@
+package model
+
+import "RandomChatting_Server/database"
+
+// UpdateImage : 멤버 찾기
+func UpdateImage(Name, Pw, fileName string) error {
+	Member := &database.Member{}
+	err := database.DB.Model(Member).Where("Name = ? AND Pw = ?", Name, Pw).Update("image", fileName).Error
+	return err
+}
