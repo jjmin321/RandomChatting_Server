29528c1621a5317f7bc9bbd29172fe979481b71d
jjmin321 <jjmin321@naver.com>
2020-09-21 19:54:57 +0900

    [ADD] lib/jwt

---
+++ lib/jwt/jwt.go
@@ -0,0 +1,70 @@
+package jwt
+
+import (
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/labstack/echo"
+)
+
+type jwtMethod interface {
+	CreateRefreshToken()
+	CreateAccessToken()
+	VerifyRefreshToken()
+	VerifyAccessToken()
+}
+
+// CreateRefreshToken : 리프레시 토큰 생성
+func CreateRefreshToken(Name, Pw string) (string, error) {
+	refreshToken := jwt.New(jwt.SigningMethodHS256)
+	claims := refreshToken.Claims.(jwt.MapClaims)
+	claims["Name"] = Name
+	claims["Pw"] = Pw
+	claims["exp"] = time.Now().Add(time.Hour * 720).Unix()
+
+	t, err := refreshToken.SignedString([]byte("secret"))
+	if err != nil {
+		return "", err
+	}
+	return t, nil
+}
+
+// CreateAccessToken : 액세스 토큰 생성
+func CreateAccessToken(Name, Pw string) (string, error) {
+	accessToken := jwt.New(jwt.SigningMethodHS256)
+	claims := accessToken.Claims.(jwt.MapClaims)
+	claims["Name"] = Name
+	claims["Pw"] = Pw
+	claims["exp"] = time.Now().Add(time.Hour * 1).Unix()
+	t, err := accessToken.SignedString([]byte("secret"))
+	if err != nil {
+		return "", err
+	}
+	return t, nil
+}
+
+// VerifyRefreshToken : 리프레시 토큰 검증
+func VerifyRefreshToken(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		token := c.Get("user").(*jwt.Token)
+		claims := token.Claims.(jwt.MapClaims)
+		Name := claims["Name"].(string)
+		Pw := claims["Pw"].(string)
+		c.Set("Name", Name)
+		c.Set("Pw", Pw)
+		return next(c)
+	}
+}
+
+// VerifyAccessToken : 액세스 토큰 검증
+func VerifyAccessToken(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		token := c.Get("user").(*jwt.Token)
+		claims := token.Claims.(jwt.MapClaims)
+		Name := claims["Name"].(string)
+		Pw := claims["Pw"].(string)
+		c.Set("ID", ID)
+		c.Set("Pw", Pw)
+		return next(c)
+	}
+}
