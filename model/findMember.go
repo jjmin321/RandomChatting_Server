d40b99a6a5f658a5020cf6813498a1170e981244
jjmin321 <jjmin321@naver.com>
2020-09-21 20:23:18 +0900

    [UPDATE] model/findMember.go

--- model/findMember.go
+++ model/findMember.go
@@ -2,9 +2,9 @@ package model
 
 import "RandomChatting_Server/database"
 
-// FindMember - 멤버 존재 여부 확인
-func FindMember(name string) error {
+// FindMember : 멤버 찾기
+func FindMember(Name, Pw string) error {
 	Member := &database.Member{}
-	err := database.DB.Where("Name = ?", name).Find(Member).Error
+	err := database.DB.Where("Name = ? AND Pw = ?", Name, Pw).Find(Member).Error
 	return err
 }
