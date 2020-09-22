7dd0d55d45feb18d371a27bfb20d075e286955ac
jjmin321 <jjmin321@naver.com>
2020-09-22 10:25:42 +0900

    [MODIFY] Return error -> Return *database.Member, error

--- model/findMember.go
+++ model/findMember.go
@@ -3,8 +3,8 @@ package model
 import "RandomChatting_Server/database"
 
 // FindMember : 멤버 찾기
-func FindMember(Name, Pw string) error {
+func FindMember(Name, Pw string) (*database.Member, error) {
 	Member := &database.Member{}
 	err := database.DB.Where("Name = ? AND Pw = ?", Name, Pw).Find(Member).Error
-	return err
+	return Member, err
 }
