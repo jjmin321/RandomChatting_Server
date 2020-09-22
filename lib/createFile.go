f8cba2c272ce0f40cb759620eaa46868dbbbaf29
jjmin321 <jjmin321@naver.com>
2020-09-22 16:47:46 +0900

    [ADD] Return value fileName

--- lib/createFile.go
+++ lib/createFile.go
@@ -12,10 +12,10 @@ type createFileMethod interface {
 }
 
 // CreateFile - 파일 생성
-func CreateFile(Name string, file *multipart.FileHeader) (*os.File, error) {
+func CreateFile(Name string, file *multipart.FileHeader) (*os.File, string, error) {
 	t := time.Now()
 	now := strconv.Itoa(int(t.Unix()))
 	fileName := Name + now + file.Filename
 	dst, err := os.Create("image/" + fileName)
-	return dst, err
+	return dst, fileName, err
 }
