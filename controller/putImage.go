4f5bdda55a275913bcc19e0ae791186463d2c3ed
jjmin321 <jjmin321@naver.com>
2020-09-22 15:43:06 +0900

    [ADD] putImage

--- controller/putImage.go
+++ controller/putImage.go
@@ -1,11 +1,9 @@
 package controller
 
 import (
+	"RandomChatting_Server/lib"
 	"RandomChatting_Server/model"
 	"io"
-	"os"
-	"strconv"
-	"time"
 
 	"github.com/labstack/echo"
 )
@@ -19,9 +17,6 @@ func PutImage(c echo.Context) error {
 	Name := c.Get("Name").(string)
 	Pw := c.Get("Pw").(string)
 	file, err := c.FormFile("image")
-	t := time.Now()
-	now := strconv.Itoa(int(t.Unix()))
-	fileName := Name + now + file.Filename
 	if err != nil {
 		return c.JSON(400, map[string]interface{}{
 			"status":  400,
@@ -36,7 +31,7 @@ func PutImage(c echo.Context) error {
 			"message": "파일을 여는 데 실패하였습니다. 다시 시도해주세요.",
 		})
 	}
-	dst, err := os.Create("image/" + fileName)
+	dst, err := lib.CreateFile(Name, file)
 	defer dst.Close()
 	if err != nil {
 		return c.JSON(500, map[string]interface{}{
@@ -50,7 +45,7 @@ func PutImage(c echo.Context) error {
 			"message": "파일을 저장하는 데 실패하였습니다. 다시 시도해주세요.",
 		})
 	}
-	err = model.UpdateImage(Name, Pw, fileName)
+	err = model.UpdateImage(Name, Pw, dst.Name())
 	if err != nil {
 		return c.JSON(500, map[string]interface{}{
 			"status":  500,
