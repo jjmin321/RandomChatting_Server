c56aa6746364bd0930f7ea4614f23809c3546e1d
jjmin321 <jjmin321@naver.com>
2020-09-22 09:44:31 +0900

    [ADD] /putImage Router

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"RandomChatting_Server/controller"
 	"RandomChatting_Server/controller/chattingserver"
 	"RandomChatting_Server/database"
+	"RandomChatting_Server/lib/jwt"
 
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 	"github.com/labstack/echo"
@@ -27,5 +28,6 @@ func main() {
 	e.GET("/chatting", chattingserver.Socket)
 	e.POST("/signIn", controller.SignIn)
 	e.POST("/signUp", controller.SignUp)
+	e.PUT("/putImage", controller.PutImage, middleware.JWT([]byte("secret")), jwt.VerifyAccessToken)
 	e.Logger.Fatal(e.Start(":80"))
 }
