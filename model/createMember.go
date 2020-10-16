cda25312838489b0631a00cf63ab98ddb2d92078
jjmin321 <jjmin321@naver.com>
2020-10-16 15:49:26 +0900

    [ADD] ID attribute

--- model/createMember.go
+++ model/createMember.go
@@ -3,8 +3,8 @@ package model
 import "RandomChatting_Server/database"
 
 // CreateMember - 멤버 생성
-func CreateMember(name, pw string) error {
-	Member := &database.Member{Name: name, Pw: pw}
+func CreateMember(id, name, pw string) error {
+	Member := &database.Member{ID: id, Name: name, Pw: pw}
 	err := database.DB.Create(Member).Error
 	return err
 }
